91dcd31d70af8c6f2228108d5b511640f3316cf1
Bartosz Sadecki <bartosz.sadecki@posteo.pl>
2026-04-03 14:37:52 +0200

    fix lint issues
    * make task names start with uppercase
    
    * switch to FQCN for module names
    
    * fix lint issues: file-sync
    
    * fix lint issues: packages
    
    * fix lint issues: config
    
    * fix lint issues: git-repos
    
    * fix lint issues: network
    
    * fix lint issues: services

--- roles/config/files/set_dav_password.py
+++ roles/config/files/set_dav_password.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 import os
+
 import keyring
 
 dav_user = os.getenv("DAV_USER")
 dav_password = os.getenv("DAV_PASSWORD")
 
-current_password = keyring.get_password('vdirsyncer', dav_user)
+if dav_user is None:
+    raise RuntimeError("DAV_USER is not defined")
+if dav_password is None:
+    raise RuntimeError("DAV_PASSWORD is not defined")
+
+current_password = keyring.get_password("vdirsyncer", dav_user)
 
 if current_password != dav_password:
-    keyring.set_password('vdirsyncer', dav_user, dav_password)
-    print('password updated')
+    keyring.set_password("vdirsyncer", dav_user, dav_password)
+    print("password updated")
